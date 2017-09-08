e97f725a0f78cc162d609f28011d573add98b0af
hmangal1 <hmangal615@gmail.com>
2017-09-08 12:24:11 -0400

    Solution to the Mad Libs exercise.

--- madlib.py
+++ madlib.py
@@ -1,5 +1,8 @@
 def main():
-    noun=eval(input("Enter a noun: "))
-    print (noun)
+    noun=input("Enter a noun: ")
+    verb=input("Enter a verb: ")
+    adjective=input("Enter an adjective: ")
+    place=input("Enter a place: ")
+    print("Take your "+adjective+" "+noun+" and "+verb+" it at the "+place+"!")
 main()
     
